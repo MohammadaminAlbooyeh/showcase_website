dcbc5bdc7212bda10bbed30da417ad7c1687c9d5
Amin Albooyeh <amin.albooyeh@gmail.com>
2026-03-02 12:26:03 +0100

    ci: enable Next static export (output: 'export'), remove export script, sync frontend/out to S3

--- frontend/next.config.cjs
+++ frontend/next.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     domains: ['localhost'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
